7791dfdeedfe6649761ca41697aa257a5136533a
KristineGH <kristina.gharibyan@mail.ru>
2020-07-10 11:16:18 +0400

    first commit

---
+++ java/myjava.js
@@ -0,0 +1,21 @@
+let a = 10
+let b = 2
+
+var sum= a+b
+console.log(sum);
+
+var difference = a-b
+console.log(difference);
+
+var product= a*b
+console.log(product)
+
+var quotient= a/b
+console.log(quotient);
+
+var reminder= a%b
+console.log(reminder);
+
+
+
+
